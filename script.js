65bcec05cd9335af6f4809d0a7fa8799bf33cf9a
Overlander <anuragnandal@hotmail.com>
2025-11-21 19:13:45 -0500

    Add score variables and playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let getRandomNumber = () => Math.floor(Math.random() * 3);
   let randomNumber = getRandomNumber();
@@ -13,4 +16,30 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let humanChoice = prompt("Rock, paper, scissors?", '');
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore += 1;
+    return console.log("You win, rock beats scissors");
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore += 1;
+    return console.log("You win, scissors beats paper");
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore += 1;
+    return console.log("You win, paper beats rock");
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore += 1;
+    return console.log("You lose, rock beats scissors");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore += 1;
+    return console.log("You lose, scissors beats paper");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1;
+    return console.log("You lose, paper beats rock");
+  } else if (humanChoice === computerChoice) {
+    return console.log("Tie");
+  }
 }
